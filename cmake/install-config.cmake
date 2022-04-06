747c079e829710cf6262209103aa3c2f000b818c
Poh Zhi-Ee <ZhiEe-Poh@ecosysgrp.com>
2022-04-06 18:30:34 +0800

    Fix find_dependency of DART

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -1,6 +1,6 @@
 include(CMakeFindDependencyMacro)
 find_dependency(Eigen3)
 find_dependency(spdlog)
-find_dependency(DART)
+find_dependency(DART COMPONENTS utils-urdf gui-osg collision-ode)
 
 include("${CMAKE_CURRENT_LIST_DIR}/DartRobotsTargets.cmake")
