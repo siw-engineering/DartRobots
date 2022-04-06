a8ac6b491bcf4a2c618909122e242f7a7a8e56f5
Poh Zhi-Ee <ZhiEe-Poh@ecosysgrp.com>
2022-04-06 12:22:23 +0800

    Fix gitignore, fix python package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from skbuild import setup
 
 setup(
-    name="Underdog",
+    name="DartRobots",
     version="0.1.0",
-    description="Project 1",
+    description="A collection of robots using dartsim",
     author="Poh Zhi Ee",
     license="MIT",
-    packages=['Underdog'],
+    packages=['DartRobots'],
     cmake_args=['-DPACKAGE_PYTHON=TRUE'],
     package_dir={'': ''},
     python_requires='>=3.8'
