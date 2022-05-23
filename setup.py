391b36d138133fc968441a9d5732963364332d88
Poh Zhi-Ee <ZhiEe-Poh@ecosysgrp.com>
2022-05-23 09:16:24 +0800

    Increase default ground size

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="DartRobots",
-    version="0.2.4",
+    version="0.2.5",
     description="A collection of robots using dartsim",
     author="Poh Zhi Ee",
     license="MIT",
